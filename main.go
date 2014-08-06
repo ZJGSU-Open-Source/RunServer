ca49857e7529a1efe5ea75ecb55cc82d71afcd9c
sakeven <jc5930@sina.cn>
2014-08-06 16:39:52 +0800

    change

--- main.go
+++ main.go
@@ -16,15 +16,6 @@ import (
 	"time"
 )
 
-func main() {
-	var sid = flag.Int("sid", -1, "solution id")
-	var timeLimit = flag.Int("time", -1, "time limit")
-	var memoryLimit = flag.Int("memory", -1, "memory limit")
-	flag.Parse()
-	log.Println("asdasd")
-	judge(*sid, *memoryLimit, *timeLimit)
-}
-
 type solution struct {
 	Sid int `json:"sid"bson:"sid"`
 
@@ -45,6 +36,46 @@ type solution struct {
 	Create string `json:"create"bson:"create"`
 }
 
+func main() {
+	var sid = flag.Int("sid", -1, "solution id")
+	var timeLimit = flag.Int("time", -1, "time limit")
+	var memoryLimit = flag.Int("memory", -1, "memory limit")
+	flag.Parse()
+
+	response, err := http.Post(config.PostHost+"/solution/detail/sid/"+strconv.Itoa(*sid), "application/json", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	var one solution
+
+	if response.StatusCode == 200 {
+		err = LoadJson(response.Body, &one)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+	pwd, err := os.Getwd()
+
+	cmd := exec.Command("mkdir", "/home/sake/run/"+strconv.Itoa(one.Sid))
+	cmd.Run()
+
+	cmd = exec.Command("cp", "-r", "/home/sake/data/"+strconv.Itoa(one.Pid), "/home/sake/run/"+strconv.Itoa(one.Sid))
+	cmd.Run()
+
+	os.Chdir("/home/sake/run/" + strconv.Itoa(one.Sid) + "/" + strconv.Itoa(one.Pid)) //
+	defer os.Chdir(pwd)
+
+	one.files()
+	defer os.RemoveAll("/home/sake/run/" + strconv.Itoa(one.Sid))
+
+	one.judge(*memoryLimit, *timeLimit)
+}
+
 func PostReader(i interface{}) (r io.Reader, err error) {
 	b, err := json.Marshal(i)
 	if err != nil {
@@ -59,32 +90,26 @@ func LoadJson(r io.Reader, i interface{}) (err error) {
 	return
 }
 
-func judge(sid, memorylimit, timelimit int) {
-	response, err := http.Post(config.PostHost+"/solution/detail/sid/"+strconv.Itoa(sid), "application/json", nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer response.Body.Close()
-	var one solution
-	if response.StatusCode == 200 {
-		err = LoadJson(response.Body, &one)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	}
-	one.SJudge(memorylimit, timelimit, "sample.in", "sample.out")
-	if one.Judge == config.JudgeAC {
-		one.SJudge(memorylimit, timelimit, "test.in", "test.out")
+func (this *solution) judge(memoryLimit, timeLimit int) {
+
+	this.compile()
+	if this.Judge != config.JudgeCE {
+		this.Judge = config.JudgeRJ
+		this.update()
+		//TODO:should get all test files.
+		//this.RunJudge(memorylimit, timelimit, "sample.in", "sample.out")
+		//if one.Judge == config.JudgeAC {
+		this.RunJudge(memoryLimit, timeLimit, "test.in", "test.out")
+		//}
 	}
 
 	action := "submit"
-	if one.Judge == config.JudgeAC {
+	if this.Judge == config.JudgeAC {
 		action = "solve"
 	}
+
 	///count if the problem has been solved
-	response, err = http.Post(config.PostHost+"/solution/count/pid/"+strconv.Itoa(one.Pid)+"/uid/"+one.Uid+"/action/solve", "application/json", nil)
+	response, err := http.Post(config.PostHost+"/solution/count/pid/"+strconv.Itoa(this.Pid)+"/uid/"+this.Uid+"/action/solve", "application/json", nil)
 	defer response.Body.Close()
 	if err != nil {
 		log.Println(err)
@@ -101,34 +126,25 @@ func judge(sid, memorylimit, timelimit int) {
 
 	}
 	///end count
+
 	if c["count"] >= 1 && action == "solve" { //当结果正确且不是第一次提交，只记录为提交而不记录为solve
 		action = "submit"
 	}
-	response, err = http.Post(config.PostHost+"/user/record/uid/"+one.Uid+"/action/"+action, "application/json", nil)
+	response, err = http.Post(config.PostHost+"/user/record/uid/"+this.Uid+"/action/"+action, "application/json", nil)
 	defer response.Body.Close()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	response, err = http.Post(config.PostHost+"/problem/record/pid/"+strconv.Itoa(one.Pid)+"/action/"+action, "application/json", nil)
+	response, err = http.Post(config.PostHost+"/problem/record/pid/"+strconv.Itoa(this.Pid)+"/action/"+action, "application/json", nil)
 	defer response.Body.Close()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	reader, err := PostReader(&one)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	response, err = http.Post(config.PostHost+"/solution/update/sid/"+strconv.Itoa(one.Sid), "application/json", reader)
-	defer response.Body.Close()
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	this.update()
 }
 
 func compare(std_out, user_out string) int {
@@ -139,39 +155,65 @@ func compare(std_out, user_out string) int {
 	}
 }
 
-/*
-一段c程序，获得pid程序的运行内存，示例：m_vmpeak = get_proc_status(pid, "VmPeak:");
-int get_proc_status(int pid, const char * mark)
-{
-    FILE * pf;
-    char fn[BUFFER_SIZE], buf[BUFFER_SIZE];
-    int ret = 0;
-    sprintf(fn, "/proc/%d/status", pid);
-    pf = fopen(fn, "r");
-    int m = strlen(mark);
-    while (pf && fgets(buf, BUFFER_SIZE - 1, pf))
-    {
+//一段c程序，获得pid程序的运行内存，示例：m_vmpeak = get_proc_status(pid, "VmPeak:");
+// func get_proc_status(pid int, mark string) int {
+//
 
-        buf[strlen(buf) - 1] = 0;
-        if (strncmp(buf, mark, m) == 0)
-        {
-            sscanf(buf + m + 1, "%d", &ret);
-        }
-    }
-    if (pf)
-        fclose(pf);
-    return ret;
+// 	var buf bytes.Buffer
+// 	var ret int
+
+// 	fn := "/proc/" + strconv.Itoa(pid) + "/status"
+// 	pf := os.Open(fn)
+//	defer pf.close()
+//
+// 	m := len(mark)
+// 	for pf && fgets(buf, BUFFER_SIZE-1, pf) {
+// 		buf[strlen(buf)-1] = 0
+// 		if strncmp(buf, mark, m) == 0 {
+// 			sscanf(buf+m+1, "%d", &ret)
+// 		}
+// 	}
+
+// 	return ret
+// }
+
+func (this *solution) compile() {
+	//begin compile source code
+
+	cmd := exec.Command("g++", "Main.cpp", "-o", "Main", "-O2", "-Wall", "-lm", "--static", "-DONLINE_JUDGE") //compile
+	err := cmd.Run()
+	if err != nil {
+		log.Println(err)
+		this.Judge = config.JudgeCE //compiler error
+		return
+	}
+
+	//end compile source code
 }
-*/
 
-func (this *solution) SJudge(memorylimit, timelimit int, infilename, outfilename string) {
+func (this *solution) files() {
+	//begin write code to file
+	fl, err := os.Open("Main.cpp")
+	for err == nil {
+		fl.Close()
+		fl, err = os.Open("Main.cpp")
+	}
+	codefile, err := os.Create("Main.cpp")
+	defer codefile.Close()
+
+	_, err = codefile.WriteString(this.Code)
+	if err != nil {
+		log.Println("source code writing to file failed")
+	}
+	//end write code to file
+}
+
+func (this *solution) RunJudge(memorylimit, timelimit int, infilename, outfilename string) {
 	log.Println("run solution")
 	this.Judge = config.JudgePD
-	pwd, err := os.Getwd()
-	os.Chdir("/home/data/ACM/" + strconv.Itoa(this.Pid))//be careful.
-	defer os.Chdir(pwd)
+
 	std_in, std_out_f := os.Stdin, os.Stdout
-	std_in, err = os.Open(infilename)
+	std_in, err := os.Open(infilename)
 	if err != nil {
 		log.Println("Open std_in file failed")
 		return
@@ -200,47 +242,7 @@ func (this *solution) SJudge(memorylimit, timelimit int, infilename, outfilename
 	defer std_out_f.Close()
 	//end read the std_out text
 
-	//begin write code to file
-	fl, err := os.Open("test.cpp")
-	for err == nil {
-		fl.Close()
-		fl, err = os.Open("test.cpp")
-	}
-	codefile, err := os.Create("test.cpp")
-	defer func() {
-		codefile.Close()
-		os.Remove("test.cpp")
-	}()
-	_, err = codefile.WriteString(this.Code)
-	if err != nil {
-		log.Println("source code writing to file failed")
-	}
-	//end write code to file
-
-	//begin compile source code
 	var out bytes.Buffer
-	cmd := exec.Command("g++", "test.cpp", "-o", "Main", "-O2", "-Wall", "-lm", "--static", "-DONLINE_JUDGE") //compile
-	err = cmd.Run()
-	if err != nil {
-		log.Println(err)
-		this.Judge = config.JudgeCE //compiler error
-		return
-	}
-	defer os.Remove("Main")
-	//end compile source code
-
-	target, err := os.Open("Main") //Memroy of the target
-	defer target.Close()
-	t_info, err := target.Stat()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if this.Memory = int(t_info.Size()) / (1024 * 1024); this.Memory > memorylimit {
-		this.Judge = config.JudgeMLE
-		return
-	}
-
 	//begin run
 	channel := make(chan int)
 	//process_id := make(chan int)
@@ -248,11 +250,9 @@ func (this *solution) SJudge(memorylimit, timelimit int, infilename, outfilename
 	bcmd := exec.Command("./Main")
 	bcmd.Stdin = std_in
 	bcmd.Stdout = &out
-	bcmd.Start()
 	go func() {
-		t_time := time.Now()
+		bcmd.Start()
 		err := bcmd.Wait()
-		this.Time = int(time.Since(t_time).Seconds() * 1000)
 		if err != nil {
 			channel <- config.JudgeRE //runtime error
 			return
@@ -264,15 +264,34 @@ func (this *solution) SJudge(memorylimit, timelimit int, infilename, outfilename
 	select {
 	case res := <-channel:
 		if res == config.JudgeCP {
-			this.Time = bcmd.ProcessState.SystemTime() + bcmd.ProcessState.UserTime()
+			this.Time = int(bcmd.ProcessState.SystemTime()+bcmd.ProcessState.UserTime()) / 1000000
+			if this.Time >= timelimit {
+				this.Time = timelimit
+				this.Judge = config.JudgeTLE
+				return
+			}
 			user_out := out.String()
 			this.Judge = compare(std_out, user_out)
 		}
-	case <-time.After(time.Second * 1):
+	case <-time.After(time.Second * time.Duration(timelimit/1000)):
 		bcmd.Process.Kill()
 		<-channel
 		this.Judge = config.JudgeTLE
 		this.Time = timelimit
 	}
 	return
+}
+
+func (this *solution) update() {
+	reader, err := PostReader(this)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	response, err := http.Post(config.PostHost+"/solution/update/sid/"+strconv.Itoa(this.Sid), "application/json", reader)
+	defer response.Body.Close()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
